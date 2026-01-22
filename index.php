8c5746f9da80100aef2d198804a4ee647806d80a
dreyn0721 <47095704+dreyn0721@users.noreply.github.com>
2026-01-22 21:47:00 +0800

    skeleton

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php 
+// Vars
+$page = "home";
+$pagetitle = "FREE Ultimate Homebuyer’s Guide | Junk Removal";
+$description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum non euismod dolor. Integer sapien ipsum, dapibus eget bibendum sed nullam sodales.";
+
+
+include("template-parts/header.php");
+?>
+
+
+
+
+xxxxx
+
+<?php include("template-parts/footer.php"); ?>
